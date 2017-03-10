8a4c5de74094b8d064f0be07a18e79b603ac6a17
Brian Mearns <bmearns@commercehub.com>
2017-03-09 22:08:17 -0500

    A few more tests around promises in then

--- test/_examples.spec.js
+++ test/_examples.spec.js
@@ -1,5 +1,8 @@
+// Support
 import {expect} from 'chai';
+import Promise from 'bluebird';
 
+// Module under test
 import * as testFramework from '../src';
 
 describe('Examples', () => {
@@ -31,4 +34,35 @@ describe('Examples', () => {
                 expect(testResults.numberOfTests).to.equal(NUMBER_OF_TESTS);
             });
     });
+
+    it('invoking a single test that fails with a rejected promise', () => {
+        return testFramework.regarding('something', (regardingSomething) => {
+            regardingSomething.itShould('do something', ({then}) => {
+                const DELAY_MS = 10;
+                then(() => Promise.delay(DELAY_MS).then(() => Promise.reject(new Error('test-error'))));
+            });
+        })
+            .run()
+            .then((testResults) => {
+                const NUMBER_OF_TESTS = 1;
+                expect(testResults.passed).to.be.false;
+                expect(testResults.numberOfTests).to.equal(NUMBER_OF_TESTS);
+            });
+    });
+
+    it('invoking a single test that passed with a fulfilled promise', () => {
+        return testFramework.regarding('something', (regardingSomething) => {
+            regardingSomething.itShould('do something', ({then}) => {
+                const DELAY_MS = 10;
+                then(() => Promise.delay(DELAY_MS));
+            });
+        })
+            .run()
+            .then((testResults) => {
+                const NUMBER_OF_TESTS = 1;
+                expect(testResults.passed).to.be.true;
+                expect(testResults.numberOfTests).to.equal(NUMBER_OF_TESTS);
+            });
+    });
+
 });
