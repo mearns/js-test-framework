6aef9028574da0c74012045b81124f6cad4c30d0
Brian Mearns <bmearns@commercehub.com>
2018-06-28 15:43:44 -0400

    Cleanup tests a bit

--- test/src/examples.spec.js
+++ test/src/examples.spec.js
@@ -21,33 +21,24 @@ function itShouldFail (when, testDescriptor) {
   })
 }
 
+
+const NOOP = () => {}
+const ERROR_THROWING_ASSERTION = () => { throw new Error('test-error')}
+
+// This is assumed, and proven in a unit test below.
+const PASSING_ASSERTION = NOOP
+
+// This is assumed, and proven in a unit test below.
+const FAILING_ASSERTION = ERROR_THROWING_ASSERTION
+
 describe('some example test specs', () => {
   describe('basic assertion rules', () => {
     itShouldPass('if the assertion function is a noop', ({that}) => {
-      that(() => {})
-    })
-
-    itShouldPass('if two chained assertions are noops', ({that}) => {
-      that(() => {})
-        .and.that(() => {})
-    })
-
-    itShouldPass('if two chained assertions are noops (2)', ({that}) => {
-      that(() => {}).and(() => {})
-    })
-
-    itShouldFail('if the second chained assertion fails', ({that}) => {
-      that(() => {}).and.that(() => { throw new Error('test-error') })
-    })
-
-    itShouldFail('if the first chained assertion fails', ({that}) => {
-      that(() => { throw new Error('test-error') }).and.that(() => {})
+      that(NOOP)
     })
 
     itShouldFail('if the assertion function throws an error', ({that}) => {
-      that(() => {
-        throw new Error('test-error')
-      })
+      that(ERROR_THROWING_ASSERTION)
     })
 
     itShouldFail('if the assertion function includes a failed chai expectation', ({that}) => {
@@ -56,6 +47,38 @@ describe('some example test specs', () => {
 
     itShouldFail('if the assertion function returns a promise that rejects', ({that}) => {
       that(() => Promise.reject(new Error('test-error')))
+    })
+
+    itShouldPass('if two chained assertions pass', ({that}) => {
+      that(PASSING_ASSERTION)
+        .and.that(PASSING_ASSERTION)
+    })
+
+    itShouldPass('if two chained assertions pass, using "and" as a method', ({that}) => {
+      that(PASSING_ASSERTION).and(PASSING_ASSERTION)
+    })
+
+    itShouldFail('if the second chained assertion fails', ({that}) => {
+      that(PASSING_ASSERTION).and.that(FAILING_ASSERTION)
+    })
+
+    itShouldFail('if the first chained assertion fails', ({that}) => {
+      that(FAILING_ASSERTION).and.that(PASSING_ASSERTION)
+    })
+
+    itShouldPass('if two unchained assertions pass', ({that}) => {
+      that(PASSING_ASSERTION)
+      that(PASSING_ASSERTION)
+    })
+
+    itShouldFail('if the first unchained assertions fails', ({that}) => {
+      that(FAILING_ASSERTION)
+      that(PASSING_ASSERTION)
+    })
+
+    itShouldFail('if the second unchained assertions fails', ({that}) => {
+      that(PASSING_ASSERTION)
+      that(FAILING_ASSERTION)
     })
   })
 })
